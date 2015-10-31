94e060e7f63e6f7d89b8d5fd8ebf2ac65fb121f8
rellefsen <ellefra@gmail.com>
2015-10-31 00:51:28 -0600

    Randy: Form now refreshes after open. Need Warning DIalog after close attempt on changed SS. Need comments. Need Extra Feature.

---
+++ PS6/SpreadsheetGUI/sheet.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SS
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.fileMenuStrip = new System.Windows.Forms.MenuStrip();
+         this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+         this.cellNameTextBox = new System.Windows.Forms.ToolStripTextBox();
+         this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+         this.cellValueTextBox = new System.Windows.Forms.ToolStripTextBox();
+         this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+         this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+         this.cellContentsTextBox = new System.Windows.Forms.ToolStripTextBox();
+         this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+         this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
+         this.enterButton = new System.Windows.Forms.Button();
+         this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+         this.spreadsheetUsageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.fileMenuStrip.SuspendLayout();
+         this.toolStrip1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // fileToolStripMenuItem
+         // 
+         this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+         this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+         this.fileToolStripMenuItem.Text = "File";
+         // 
+         // newToolStripMenuItem
+         // 
+         this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+         this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.newToolStripMenuItem.Text = "New";
+         this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+         // 
+         // openToolStripMenuItem
+         // 
+         this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+         this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.openToolStripMenuItem.Text = "Open";
+         this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+         // 
+         // saveToolStripMenuItem
+         // 
+         this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+         this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.saveToolStripMenuItem.Text = "Save";
+         this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+         // 
+         // closeToolStripMenuItem
+         // 
+         this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+         this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+         this.closeToolStripMenuItem.Text = "Close";
+         this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+         // 
+         // helpToolStripMenuItem
+         // 
+         this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spreadsheetUsageToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+         this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+         this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+         this.helpToolStripMenuItem.Text = "Help";
+         // 
+         // fileMenuStrip
+         // 
+         this.fileMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+         this.fileMenuStrip.Location = new System.Drawing.Point(0, 0);
+         this.fileMenuStrip.Name = "fileMenuStrip";
+         this.fileMenuStrip.Size = new System.Drawing.Size(704, 24);
+         this.fileMenuStrip.TabIndex = 1;
+         this.fileMenuStrip.Text = "menuStrip1";
+         // 
+         // toolStripLabel1
+         // 
+         this.toolStripLabel1.Name = "toolStripLabel1";
+         this.toolStripLabel1.Size = new System.Drawing.Size(62, 22);
+         this.toolStripLabel1.Text = "Cell Name";
+         // 
+         // cellNameTextBox
+         // 
+         this.cellNameTextBox.Name = "cellNameTextBox";
+         this.cellNameTextBox.ReadOnly = true;
+         this.cellNameTextBox.Size = new System.Drawing.Size(25, 25);
+         this.cellNameTextBox.Text = "A1";
+         // 
+         // toolStripLabel2
+         // 
+         this.toolStripLabel2.Name = "toolStripLabel2";
+         this.toolStripLabel2.Size = new System.Drawing.Size(58, 22);
+         this.toolStripLabel2.Text = "Cell Value";
+         // 
+         // cellValueTextBox
+         // 
+         this.cellValueTextBox.Name = "cellValueTextBox";
+         this.cellValueTextBox.ReadOnly = true;
+         this.cellValueTextBox.Size = new System.Drawing.Size(100, 25);
+         // 
+         // toolStripSeparator
+         // 
+         this.toolStripSeparator.Name = "toolStripSeparator";
+         this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+         // 
+         // toolStripLabel3
+         // 
+         this.toolStripLabel3.Name = "toolStripLabel3";
+         this.toolStripLabel3.Size = new System.Drawing.Size(78, 22);
+         this.toolStripLabel3.Text = "Cell Contents";
+         // 
+         // cellContentsTextBox
+         // 
+         this.cellContentsTextBox.Name = "cellContentsTextBox";
+         this.cellContentsTextBox.Size = new System.Drawing.Size(100, 25);
+         this.cellContentsTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.input_KeyDown);
+         // 
+         // toolStrip1
+         // 
+         this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+         this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.cellNameTextBox,
+            this.toolStripLabel2,
+            this.cellValueTextBox,
+            this.toolStripSeparator,
+            this.toolStripLabel3,
+            this.cellContentsTextBox});
+         this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+         this.toolStrip1.Name = "toolStrip1";
+         this.toolStrip1.Size = new System.Drawing.Size(704, 25);
+         this.toolStrip1.TabIndex = 2;
+         this.toolStrip1.Text = "toolStrip1";
+         // 
+         // spreadsheetPanel1
+         // 
+         this.spreadsheetPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.spreadsheetPanel1.Location = new System.Drawing.Point(0, 52);
+         this.spreadsheetPanel1.Name = "spreadsheetPanel1";
+         this.spreadsheetPanel1.Size = new System.Drawing.Size(704, 392);
+         this.spreadsheetPanel1.TabIndex = 3;
+         // 
+         // enterButton
+         // 
+         this.enterButton.Location = new System.Drawing.Point(520, 26);
+         this.enterButton.Name = "enterButton";
+         this.enterButton.Size = new System.Drawing.Size(75, 23);
+         this.enterButton.TabIndex = 4;
+         this.enterButton.Text = "Enter";
+         this.enterButton.UseVisualStyleBackColor = true;
+         this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+         // 
+         // backgroundWorker1
+         // 
+         this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+         // 
+         // spreadsheetUsageToolStripMenuItem
+         // 
+         this.spreadsheetUsageToolStripMenuItem.Name = "spreadsheetUsageToolStripMenuItem";
+         this.spreadsheetUsageToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+         this.spreadsheetUsageToolStripMenuItem.Text = "Spreadsheet Usage";
+         this.spreadsheetUsageToolStripMenuItem.Click += new System.EventHandler(this.spreadsheetUsageToolStripMenuItem_Click);
+         // 
+         // aboutToolStripMenuItem
+         // 
+         this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+         this.aboutToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+         this.aboutToolStripMenuItem.Text = "About Spreadsheet";
+         this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(704, 445);
+         this.Controls.Add(this.enterButton);
+         this.Controls.Add(this.spreadsheetPanel1);
+         this.Controls.Add(this.toolStrip1);
+         this.Controls.Add(this.fileMenuStrip);
+         this.MainMenuStrip = this.fileMenuStrip;
+         this.Name = "Form1";
+         this.Text = "Sheet";
+         this.fileMenuStrip.ResumeLayout(false);
+         this.fileMenuStrip.PerformLayout();
+         this.toolStrip1.ResumeLayout(false);
+         this.toolStrip1.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+      private System.Windows.Forms.MenuStrip fileMenuStrip;
+      private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+      private System.Windows.Forms.ToolStripTextBox cellNameTextBox;
+      private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+      private System.Windows.Forms.ToolStripTextBox cellValueTextBox;
+      private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+      private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+      private System.Windows.Forms.ToolStripTextBox cellContentsTextBox;
+      private System.Windows.Forms.ToolStrip toolStrip1;
+      private SpreadsheetPanel spreadsheetPanel1;
+      private System.Windows.Forms.Button enterButton;
+      private System.ComponentModel.BackgroundWorker backgroundWorker1;
+      private System.Windows.Forms.ToolStripMenuItem spreadsheetUsageToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+   }
+
+}
---
+++ PS6/SpreadsheetGUI/sheet.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Resources;
+using System.Text.RegularExpressions;
+using SpreadsheetUtilities;
+using System.IO;
+
+namespace SS
+{
+   /// <summary>
+   /// Example of using a SpreadsheetPanel object
+   /// </summary>
+   public partial class Form1 : Form
+   {
+      Spreadsheet mainSpreadsheet;
+      String activeCell = "A1";
+      private int col, row;
+      /// <summary>
+      /// Default Constructor for the Spreadsheet Form
+      /// </summary>
+      public Form1()
+      {
+         mainSpreadsheet = new Spreadsheet(s => Regex.IsMatch(s, "[A-Z][1-9][0-9]?$"), s => s.ToUpper(), "ps6");
+
+         InitializeComponent();
+
+         spreadsheetPanel1.SelectionChanged += selectCell;
+         spreadsheetPanel1.SetSelection(0, 0);
+      }
+
+      /// <summary>
+      /// Default Constructor for the Spreadsheet Form
+      /// </summary>
+      public Form1(String openfile)
+      {
+         mainSpreadsheet = new Spreadsheet(openfile, s => Regex.IsMatch(s, "[A-Z][1-9][0-9]?$"), s => s.ToUpper(), "ps6");
+
+         InitializeComponent();
+
+         spreadsheetPanel1.SelectionChanged += selectCell;
+         spreadsheetPanel1.SetSelection(0, 0);
+
+         refreshCells(mainSpreadsheet.GetNamesOfAllNonemptyCells());
+      }
+
+
+      private void selectCell(SpreadsheetPanel ssPanel)
+      {
+         ssPanel.GetSelection(out col, out row);
+         activeCell = getCellName(col, row);
+         cellNameTextBox.Text = activeCell;
+         cellValueTextBox.Text = mainSpreadsheet.GetCellValue(activeCell).ToString();
+         cellContentsTextBox.Text = mainSpreadsheet.GetCellContents(activeCell).ToString();
+         cellContentsTextBox.Focus();
+      }
+
+      // Deals with the New menu
+      private void newToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         // Tell the application context to run the form on the same
+         // thread as the other forms.
+         DemoApplicationContext.getAppContext().RunForm(new Form1());
+      }
+
+      private void openToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         Stream myStream = null;
+         OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+         openFileDialog1.InitialDirectory = "c:\\";
+         openFileDialog1.Filter = 
+            "sprd files (*.sprd)|*.sprd|All files (*.*)|*.*";
+         openFileDialog1.FilterIndex = 1;
+         openFileDialog1.RestoreDirectory = true;
+         openFileDialog1.InitialDirectory =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+         if (openFileDialog1.ShowDialog() == DialogResult.OK)
+         {
+            try
+            {
+                DemoApplicationContext.getAppContext().RunForm(new Form1(openFileDialog1.FileName));
+               
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show("Error: Could not read file: " + ex.Message, "Open File Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+         }
+      }
+
+      private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         // Set the properties on SaveFileDialog1 so the user is 
+         // prompted to create the file if it doesn't exist 
+         // or overwrite the file if it does exist.
+         SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+         saveFileDialog1.CreatePrompt = true;
+         saveFileDialog1.OverwritePrompt = true;
+
+         // Set the file name to myText.txt, set the type filter
+         // to sprd files, and set the initial directory to the 
+         // MyDocuments folder.
+         saveFileDialog1.FileName = "spreadsheet";
+         // DefaultExt is only used when "All files" is selected from 
+         // the filter box and no extension is specified by the user.
+         saveFileDialog1.DefaultExt = "sprd";
+         saveFileDialog1.Filter =
+             "sprd files (*.sprd)|*.sprd|All files (*.*)|*.*";
+         saveFileDialog1.InitialDirectory =
+             Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+         // Call ShowDialog and check for a return value of DialogResult.OK,
+         // which indicates that the file was saved. 
+         DialogResult result = saveFileDialog1.ShowDialog();
+
+         // save the spreadsheet
+         mainSpreadsheet.Save(saveFileDialog1.FileName);
+
+      }
+
+
+      // Deals with the Close menu
+      private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         Close();
+      }
+
+      private static String getCellName(int col, int row)
+      {
+         const String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+         String column = letters.Substring(col, 1);
+         // Increment Row so we start with Row 1
+         row++;
+         String newRow = row.ToString();
+         return column + newRow;
+      }
+
+      private static Tuple<int, int> getColRow(String cell)
+      {
+         int col, row;
+         col = (int)(cell[0] - 65);
+         int.TryParse(cell.Substring(1), out row);
+
+         return new Tuple<int, int>(col, --row);
+      }
+
+
+      private void input_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Return)
+         {
+            enterButton_Click(sender, e);
+            e.SuppressKeyPress = true; // stop the annoying bing that Windows makes when you press a key
+         }
+      }
+
+      private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+      {
+
+      }
+
+      private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         String aboutText = "SpreadSheet version 0.6\n Developed by team ellefsakishev";
+         MessageBox.Show(aboutText, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+      }
+
+      private void spreadsheetUsageToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+         String aboutText = "To use the spreadsheet: ";
+         MessageBox.Show(aboutText, "Usage", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+      }
+
+      private void refreshCells(IEnumerable<String> toUpdate)
+      {
+         Tuple<int, int> temp;
+
+         foreach (string el in toUpdate)
+         {
+
+            temp = getColRow(el);
+            object cellValue = mainSpreadsheet.GetCellValue(el);
+
+            if (cellValue is FormulaError)
+            {
+               FormulaError cellError = (FormulaError)cellValue;
+               spreadsheetPanel1.SetValue(temp.Item1, temp.Item2, "*Cell Error*");
+            }
+            else
+               spreadsheetPanel1.SetValue(temp.Item1, temp.Item2, mainSpreadsheet.GetCellValue(el).ToString());
+         }
+      }
+    
+      private void enterButton_Click(object sender, EventArgs e)
+      {
+         Tuple<int, int> temp;
+         try
+         {
+
+            ISet<string> ToUpdate = mainSpreadsheet.SetContentsOfCell(activeCell, cellContentsTextBox.Text);
+            cellValueTextBox.Text = mainSpreadsheet.GetCellValue(activeCell).ToString();
+            foreach (string el in ToUpdate)
+            {
+
+               temp = getColRow(el);
+               object cellValue = mainSpreadsheet.GetCellValue(el);
+
+               if (cellValue is FormulaError)
+               {
+                  FormulaError cellError = (FormulaError)cellValue;
+                  spreadsheetPanel1.SetValue(temp.Item1, temp.Item2, "*Cell Error*");
+                  MessageBox.Show(cellError.Reason,"Cell Error",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+               }
+               else
+                  spreadsheetPanel1.SetValue(temp.Item1, temp.Item2, mainSpreadsheet.GetCellValue(el).ToString());
+            }
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Invalid entry in " + activeCell + " \n Error:" + ex, "Cell Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+         }
+      }
+   }
+}
