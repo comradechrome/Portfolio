2ac14bcd1c3ec4810cdf5e497b6652855e593991
rellefsen <ellefra@gmail.com>
2015-11-12 02:01:02 -0700

    Randy: nearly done processing Raw JSON - added team_id to model

--- AgCubio/Model/Model.cs
+++ AgCubio/Model/Model.cs
@@ -8,201 +8,241 @@ using Newtonsoft.Json;
 
 namespace AgCubio
 {
-    /// <summary>
-    /// A cube contains:
-    /// * A unique id
-    /// * The position in space(x, y)
-    /// * A color
-    /// * A name -- if this is a player cube
-    /// * A mass
-    /// * Food status (is this cube food or a player)
-    /// 
-    /// These should be derived properties based on mass:
-    /// * Width, Top, Left, Right, Bottom
-    /// </summary>
-    public class Cube
-    {
-        private const int scalingFactor = 10;
+   /// <summary>
+   /// A cube contains:
+   /// * A unique id
+   /// * The position in space(x, y)
+   /// * A color
+   /// * A name -- if this is a player cube
+   /// * A mass
+   /// * Food status (is this cube food or a player)
+   /// 
+   /// These should be derived properties based on mass:
+   /// * Width, Top, Left, Right, Bottom
+   /// </summary>
+   public class Cube
+   {
+      private const int scalingFactor = 10;
 
-        /// <summary>
-        /// x coordinate of the center of the cube
-        /// </summary>
-        public Double loc_x { get; set; }
+      /// <summary>
+      /// x coordinate of the center of the cube
+      /// </summary>
+      public Double loc_x { get; set; }
 
-        /// <summary>
-        /// y coordinate of the center of the cube
-        /// </summary>
-        public Double loc_y { get; set; }
+      /// <summary>
+      /// y coordinate of the center of the cube
+      /// </summary>
+      public Double loc_y { get; set; }
 
-        /// <summary>
-        /// integer representation of the ARGB color value
-        /// </summary>
-        public int argb_color { get; }
+      /// <summary>
+      /// integer representation of the ARGB color value
+      /// </summary>
+      public int argb_color { get; }
 
-        /// <summary>
-        /// Cube unique ID
-        /// </summary>
-        public int uid { get; }
+      /// <summary>
+      /// Cube unique ID
+      /// </summary>
+      public int uid { get; }
 
-        /// <summary>
-        /// true = Food
-        /// false = Player
-        /// </summary>
-        public bool food { get; }
+      /// <summary>
+      /// true = Food
+      /// false = Player
+      /// </summary>
+      public bool food { get; }
 
-        /// <summary>
-        /// The name on the cube - if food, the name will be an empty string
-        /// </summary>
-        public String Name { get; }
-
-
-        /// <summary>
-        /// mass of the cube - if set attempts a value less than 1, we'll default to 1
-        /// </summary>
-        public Double Mass { get; set; }
+      /// <summary>
+      /// The name on the cube - if food, the name will be an empty string
+      /// </summary>
+      public String Name { get; }
 
 
-        /// <summary>
-        /// Width is the square root of the mass. We will drop all decimal values.
-        /// Width is a read only property
-        /// </summary>
-        public Double Width
-        {
-            get { return Math.Sqrt(Mass * scalingFactor); }
-        }
+      /// <summary>
+      /// mass of the cube - if set attempts a value less than 1, we'll default to 1
+      /// </summary>
+      public Double Mass { get; set; }
 
-        /// <summary>
-        /// Radius is half the square root of the mass. We will drop all decimal values.
-        /// Radius is a read only property
-        /// </summary>
-        public Double Radius
-        {
-            get { return this.Width / 2; }
-        }
+      /// <summary>
+      /// 
+      /// </summary>
+      public int team_id { get; set; }
 
-        /// <summary>
-        /// Default contructor of the cube object
-        /// </summary>
-        /// <param name="loc_x"></param>
-        /// <param name="loc_y"></param>
-        /// <param name="argb_color"></param>
-        /// <param name="uid"></param>
-        /// <param name="food"></param>
-        /// <param name="name"></param>
-        /// <param name="mass"></param>
-        public Cube(Double loc_x, Double loc_y, int argb_color, int uid, bool food, String name, Double mass)
-        {
-            this.loc_x = loc_x;
-            this.loc_y = loc_y;
-            this.argb_color = argb_color;
-            this.uid = uid;
-            this.food = food;
-            Name = name;
-            Mass = mass;
-        }
-    }
+      /// <summary>
+      /// Width is the square root of the mass. We will drop all decimal values.
+      /// Width is a read only property
+      /// </summary>
+      public Double Width
+      {
+         get { return Math.Sqrt(Mass * scalingFactor); }
+      }
 
-    /// <summary>
-    /// Represents 'State' of world. Responsible for tracking:
-    /// * the world Width and Height (read only 'constants')
-    /// * all the cubes in the game.
-    /// </summary>
-    public class World
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int worldHieght { get; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public int worldWidth { get; }
-        /// <summary>
-        /// Our players UID
-        /// </summary>
-        public int ourID { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public Dictionary<int, Cube> cubes { get; }
+      /// <summary>
+      /// Radius is half the square root of the mass. We will drop all decimal values.
+      /// Radius is a read only property
+      /// </summary>
+      public Double Radius
+      {
+         get { return this.Width / 2; }
+      }
+
+      /// <summary>
+      /// Default contructor of the cube object
+      /// </summary>
+      /// <param name="loc_x"></param>
+      /// <param name="loc_y"></param>
+      /// <param name="argb_color"></param>
+      /// <param name="uid"></param>
+      /// <param name="food"></param>
+      /// <param name="name"></param>
+      /// <param name="mass"></param>
+      public Cube(Double loc_x, Double loc_y, int argb_color, int uid, int team_id, bool food, String name, Double mass)
+      {
+         this.loc_x = loc_x;
+         this.loc_y = loc_y;
+         this.argb_color = argb_color;
+         this.uid = uid;
+         this.team_id = team_id;
+         this.food = food;
+         this.Name = name;
+         this.Mass = mass;
+      }
+   }
+
+   /// <summary>
+   /// Represents 'State' of world. Responsible for tracking:
+   /// * the world Width and Height (read only 'constants')
+   /// * all the worldCubes in the game.
+   /// </summary>
+   public class World
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      public int worldHieght { get; }
+      /// <summary>
+      /// 
+      /// </summary>
+      public int worldWidth { get; }
+      /// <summary>
+      /// Our players UID
+      /// </summary>
+      public int ourID { get; set; }
+      /// <summary>
+      /// 
+      /// </summary>
+      public Dictionary<int, Cube> worldCubes { get; }
+      /// <summary>
+      /// List of our worldCubes - 
+      /// </summary>
+      public Dictionary<int, Cube> ourCubes { get; }
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="hieght"></param>
-        /// <param name="width"></param>
-        /// <param name="id"></param>
-        public World(int hieght, int width)
-        {
-            this.worldHieght = hieght;
-            this.worldWidth = width;
-            cubes = new Dictionary<int, Cube>();
-        }
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="hieght"></param>
+      /// <param name="width"></param>
+      public World(int hieght, int width)
+      {
+         this.worldHieght = hieght;
+         this.worldWidth = width;
+         worldCubes = new Dictionary<int, Cube>();
+      }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="color"></param>
-        /// <param name="uid"></param>
-        /// <param name="food"></param>
-        /// <param name="name"></param>
-        /// <param name="mass"></param>
-        public void newCube(Double x, Double y, int color, int uid, bool food, String name, Double mass)
-        {
-            Cube cube = new Cube(x, y, color, uid, food, name, mass);
-            cubes.Add(uid, cube);
+      /// <summary>
+      /// Add worldCubes to our cube dictionary. Key is the cube UID. Also check the cube team ID.
+      /// IF the team ID is the same as our UID, add it to the ourCubes HashSet
+      /// </summary>
+      /// <param name="cube"></param>
+      public void addCube(Cube cube)
+      {
+         worldCubes.Add(cube.uid, cube);
+         if (cube.team_id == ourID)
+            ourCubes.Add(cube.uid,cube);
+      }
 
-        }
+      /// <summary>
+      /// TODO: do we need to modify mass and position of worldCubes in ourCubes?
+      /// </summary>
+      /// <param name="cube"></param>
+      public void moveCube(Cube cube)
+      {
+         if (worldCubes.ContainsKey(cube.uid))
+         {
+            worldCubes[cube.uid].Mass = cube.Mass;
+            worldCubes[cube.uid].loc_x = cube.loc_x;
+            worldCubes[cube.uid].loc_y = cube.loc_y;
+         }
+         // check if ourCubes contains cube, if so modify it
+         if (ourCubes.ContainsKey(cube.uid)) 
+         {
+            ourCubes[cube.uid].Mass = cube.Mass;
+            ourCubes[cube.uid].loc_x = cube.loc_x;
+            ourCubes[cube.uid].loc_y = cube.loc_y;
+         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cube"></param>
-        public void addCube(Cube cube)
-        {
-            cubes.Add(cube.uid, cube);
-        }
+         // do nothing if uid doesn't exist -
+         // TODO: may need to handle this, but we'll ignore for now - we already checked for this condition in view
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cube"></param>
-        public void moveCube(Cube cube)
-        {
-            if (cubes.ContainsKey(cube.uid))
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="cube"></param>
+      public void removeCube(Cube cube)
+      {
+         if (worldCubes.ContainsKey(cube.uid))
+            worldCubes.Remove(cube.uid);
+         if (ourCubes.ContainsKey(cube.uid)) 
+            ourCubes.Remove(cube.uid);
+      }
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="jsonCube"></param>
+      //public void firstCube(string jsonCube)
+      //{
+      //   Cube cube = JsonConvert.DeserializeObject<Cube>(jsonCube);
+      //   ourID = cube.uid;
+      //   addCube(cube);
+      //}
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="jsonCube"></param>
+      public void processCube(String jsonCube)
+      {
+         Cube cube = JsonConvert.DeserializeObject<Cube>(jsonCube);
+         // check if cube exists
+         if (worldCubes.ContainsKey(cube.uid))
+         {
+            // check if mass equals zero
+            if (cube.Mass == 0.0)
             {
-                cubes[cube.uid].Mass = cube.Mass;
-                cubes[cube.uid].loc_x = cube.loc_x;
-                cubes[cube.uid].loc_y = cube.loc_y;
+               // check if our mass is zero - end game if true
+               if (cube.uid == ourID)
+               {
+                  // TODO: run code to end game
+               }
+               else
+                  removeCube(cube);
             }
-            // do nothing if uid doesn't exist -
-            // TODO: may need to handle this, but we'll ignore for now - we already checked for this condition in view
-
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cube"></param>
-        public void removeCube(Cube cube)
-        {
-            if (cubes.ContainsKey(cube.uid))
-                cubes.Remove(cube.uid);
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="jsonCube"></param>
-        public void firstCube(string jsonCube)
-        {
-            Cube cube = JsonConvert.DeserializeObject<Cube>(jsonCube);
-            ourID = cube.uid;
+            // cube exists and is not zero mass
+            else
+               moveCube(cube);
+         }
+         // cube doesn't exist so we will add it
+         else
+         {
+            // if team_id == uid, this is our cube so we will set ourID
+            if (cube.uid == cube.team_id)
+               ourID = cube.uid;
             addCube(cube);
-        }
+         }
 
-    }
+      }
+
+   }
 
 }
--- AgCubio/View/PlayerConsole.cs
+++ AgCubio/View/PlayerConsole.cs
@@ -12,228 +12,226 @@ using System.Net.Sockets;
 
 namespace AgCubio
 {
-    public partial class PlayerConsole : Form
-    {
-        private Delegate ConnectionDelegate;
+   public partial class PlayerConsole : Form
+   {
+      private Delegate ConnectionDelegate;
 
-        private System.Drawing.SolidBrush myBrush;
-        private System.Drawing.SolidBrush textColor;
+      private System.Drawing.SolidBrush myBrush;
+      private System.Drawing.SolidBrush textColor;
 
-        World mainWorld;
-        /// <summary>
-        /// 
-        /// </summary>
-        public int foodCount = 0;
-        /// <summary>
-        /// 
-        /// </summary>
+      World mainWorld;
+      /// <summary>
+      /// 
+      /// </summary>
+      public int foodCount = 0;
+      /// <summary>
+      /// 
+      /// </summary>
       public String ourServer;
-        /// <summary>
+      /// <summary>
       /// the mass of our current cube
-        /// </summary>
+      /// </summary>
       public Double ourMass;
-        /// <summary>
-        /// 
-        /// </summary>
-        public PlayerConsole()
-        {
+      /// <summary>
+      /// String to contain the reaminder of incomplete JSON text after each buffer read
+      /// </summary>
+      private String remainingJson = "";
+      public PlayerConsole()
+      {
 
-            //TODO: get playername and server name - then hide textbox and labels
+         //TODO: get playername and server name - then hide textbox and labels
 
          // TODO: may not have UID yet
          mainWorld = new World(1000, 1000);
 
-            //temporary data test method
-            //buildWorld();
+         //temporary data test method
+         //buildWorld();
 
-            InitializeComponent();
-            DoubleBuffered = true;
+         InitializeComponent();
+         DoubleBuffered = true;
 
-            Random rand = new Random(0);
-            //while (true)
-            //{
-            //    Cube cube = new Cube(rand.NextDouble()*1000, rand.NextDouble() * 1000, (int)rand.NextDouble() * -32000000, (int)rand.NextDouble() * 5000, true, "", 1.0);
+         Random rand = new Random(0);
+         //while (true)
+         //{
+         //    Cube cube = new Cube(rand.NextDouble()*1000, rand.NextDouble() * 1000, (int)rand.NextDouble() * -32000000, (int)rand.NextDouble() * 5000, true, "", 1.0);
 
-            //}
-        }
-
-
-        /// <summary>
-        /// Temporary method to read test data and populate our world
-        /// </summary>
-        private void buildWorld()
-        {
-            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\Resources\Libraries\sample.data");
-            foreach (string line in lines)
-            {
-                Cube cube = JsonConvert.DeserializeObject<Cube>(line);
-                mainWorld.addCube(cube);
-            }
-
-        }
-
-        /// <summary>
-        /// TODO: add this.Invalidate(); to end of method
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void PlayerConsole_Paint(object sender, PaintEventArgs e)
-        {
-            foodCount = 0;
-         lock (mainWorld)
-            {
-                foreach (KeyValuePair<int, Cube> cube in mainWorld.cubes)
-                {
-                    //Need to reset food count to zero
-
-                    if (cube.Value.food)
-                        foodCount++;
-       
-               if (mainWorld.ourID == cube.Value.uid)
-                        ourMass = cube.Value.Mass;
-
-                    Color color = Color.FromArgb(cube.Value.argb_color);
-                    myBrush = new System.Drawing.SolidBrush(color);
-                    textColor = new System.Drawing.SolidBrush(Color.Black);
-                    Rectangle rectangle = new Rectangle((int)cube.Value.loc_x, (int)cube.Value.loc_y, (int)cube.Value.Width, (int)cube.Value.Width);
-                    e.Graphics.FillRectangle(myBrush, rectangle);
-                    Font myFont = new Font("Arial", 10);
-                    e.Graphics.DrawString(cube.Value.Name, myFont, textColor, (int)cube.Value.loc_x, (int)cube.Value.loc_y);
-                }
-
-                //send mouse location to server
-
-            }
-        }
-
-        private void aboutAgCubioToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void usingAgCubioToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void width_TextChanged(object sender, EventArgs e)
-        {
-            //TODO: might want to use the cube method to get this
-            width.Text = Math.Sqrt(ourMass).ToString();
-        }
-
-        private void fps_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void food_TextChanged(object sender, EventArgs e)
-        {
-            food.Text = foodCount.ToString();
-        }
-
-        private void mass_TextChanged(object sender, EventArgs e)
-        {
-            mass.Text = ourMass.ToString();
-        }
-
-        private void textBox_playerName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox_serverName_TextChanged(object sender, EventArgs e)
-        {
-            ourServer = textBox_serverName.ToString();
-            
-        }
-
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox_playerName_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Return)
-                if (!String.IsNullOrEmpty(textBox_playerName.Text) && !String.IsNullOrEmpty(textBox_serverName.Text))
-                    startGame();
-        }
-
-        private void textBox_serverName_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Return)
-                if (!String.IsNullOrEmpty(textBox_playerName.Text) && !String.IsNullOrEmpty(textBox_serverName.Text))
-                    startGame();   
-        }
-
-        private void startGame()
-        {
-            Network.Connect_to_Server(SendPlayerInfo, textBox_serverName.Text);
-        }
-
-        private static void EndConnectCallback(IAsyncResult ar)
-        {
-            StateObject state = (StateObject)ar.AsyncState;
-            state.workSocket.EndConnect(ar);
-        }
-
-        private void SendPlayerInfo(StateObject state)
-        {
-            this.Invoke(new Action(() =>
-            {
-                textBox_playerName.Hide();
-                label_playerName.Hide();
-                textBox_serverName.Hide();
-                label_serverName.Hide();
-            }));
-           
-            Network.Send(state.workSocket, textBox_playerName.Text + "\n");
-
-            state.CallbackAction = ReceiveData;
-            Network.i_want_more_data(state);
-
-        }
-
-        private void ReceiveData (StateObject state)
-        {
-            StringBuilder test = state.sb;
-            MessageBox.Show(state.sb.ToString());
-            mainWorld.firstCube(state.sb.ToString());
-            Network.i_want_more_data(state);
-        }
-
-      private void processCube(Cube cube)
-      {
-         // check if cube exists
-         if(mainWorld.cubes.ContainsKey(cube.uid))
-         {
-            // check if mass equals zero
-            if(cube.Mass == 0.0)
-            {
-               // check if our mass is zero - end game if true
-               if(cube.uid == mainWorld.ourID)
-               {
-                  // TODO: run code to end game
-               }
-               else
-                  mainWorld.removeCube(cube);
-            }
-            // cube exists and is not zero mass
-            else
-               mainWorld.moveCube(cube);
-         }
-         // cube doesn't exist so we will add it
-         else
-            mainWorld.addCube(cube);
-
+         //}
       }
 
 
-    }
+      /// <summary>
+      /// Temporary method to read test data and populate our world
+      /// </summary>
+      private void buildWorld()
+      {
+         string[] lines = System.IO.File.ReadAllLines(@"..\..\..\Resources\Libraries\sample.data");
+         foreach (string line in lines)
+         {
+            Cube cube = JsonConvert.DeserializeObject<Cube>(line);
+            mainWorld.addCube(cube);
+         }
+
+      }
+
+      /// <summary>
+      /// TODO: add this.Invalidate(); to end of method
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void PlayerConsole_Paint(object sender, PaintEventArgs e)
+      {
+         foodCount = 0;
+         lock (mainWorld)
+         {
+            foreach (KeyValuePair<int, Cube> cube in mainWorld.worldCubes)
+            {
+               //Need to reset food count to zero
+
+               if (cube.Value.food)
+                  foodCount++;
+
+               if (mainWorld.ourID == cube.Value.uid)
+                  ourMass = cube.Value.Mass;
+
+               Color color = Color.FromArgb(cube.Value.argb_color);
+               myBrush = new System.Drawing.SolidBrush(color);
+               textColor = new System.Drawing.SolidBrush(Color.Black);
+               Rectangle rectangle = new Rectangle((int)cube.Value.loc_x, (int)cube.Value.loc_y, (int)cube.Value.Width, (int)cube.Value.Width);
+               e.Graphics.FillRectangle(myBrush, rectangle);
+               Font myFont = new Font("Arial", 10);
+               e.Graphics.DrawString(cube.Value.Name, myFont, textColor, (int)cube.Value.loc_x, (int)cube.Value.loc_y);
+            }
+
+            //send mouse location to server
+
+         }
+      }
+
+      private void aboutAgCubioToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void usingAgCubioToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void width_TextChanged(object sender, EventArgs e)
+      {
+         //TODO: might want to use the cube method to get this
+         width.Text = Math.Sqrt(ourMass).ToString();
+      }
+
+      private void fps_TextChanged(object sender, EventArgs e)
+      {
+
+      }
+
+      private void food_TextChanged(object sender, EventArgs e)
+      {
+         food.Text = foodCount.ToString();
+      }
+
+      private void mass_TextChanged(object sender, EventArgs e)
+      {
+         mass.Text = ourMass.ToString();
+      }
+
+      private void textBox_playerName_TextChanged(object sender, EventArgs e)
+      {
+
+      }
+
+      private void textBox_serverName_TextChanged(object sender, EventArgs e)
+      {
+         ourServer = textBox_serverName.ToString();
+
+      }
+
+      private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+
+      }
+
+      private void textBox_playerName_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Return)
+            if (!String.IsNullOrEmpty(textBox_playerName.Text) && !String.IsNullOrEmpty(textBox_serverName.Text))
+               startGame();
+      }
+
+      private void textBox_serverName_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Return)
+            if (!String.IsNullOrEmpty(textBox_playerName.Text) && !String.IsNullOrEmpty(textBox_serverName.Text))
+               startGame();
+      }
+
+      private void startGame()
+      {
+         Network.Connect_to_Server(SendPlayerInfo, textBox_serverName.Text);
+      }
+
+      private static void EndConnectCallback(IAsyncResult ar)
+      {
+         StateObject state = (StateObject)ar.AsyncState;
+         state.workSocket.EndConnect(ar);
+      }
+
+      private void SendPlayerInfo(StateObject state)
+      {
+         this.Invoke(new Action(() =>
+         {
+            textBox_playerName.Hide();
+            label_playerName.Hide();
+            textBox_serverName.Hide();
+            label_serverName.Hide();
+         }));
+
+         Network.Send(state.workSocket, textBox_playerName.Text + "\n");
+
+         state.CallbackAction = ReceiveData;
+         Network.i_want_more_data(state);
+
+      }
+
+      private void ReceiveData(StateObject state)
+      {
+         // save our state string buffer to a new String
+         String newJson = new StringBuilder(state.sb.ToString()).ToString();
+         // clear out the state buffer
+         state.sb.Clear();
+         // process the raw JSON data and return a string array of clean JSON
+         String[] jsonLines = processJson(remainingJson + newJson);
+         // JSON is now clean, we can process line by line
+         foreach (String line in jsonLines)
+         {
+            MessageBox.Show(line);
+            mainWorld.processCube(line);
+         }
+
+         Network.i_want_more_data(state);
+
+      }
+
+      /// <summary>
+      /// TODO: process our json - extra brackets - incomplete lines etc. - return a String Array of clean JSON
+      /// TODO: save the remainder in a 'remainingJson' string to prepend to our next set of incoming data
+      /// </summary>
+      /// <param name="rawJson"></param>
+      /// <returns></returns>
+      private String[] processJson(String rawJson)
+      {
+
+
+         return null;
+      }
+
+   }
 }
